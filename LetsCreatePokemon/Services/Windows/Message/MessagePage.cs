d76331484ee20c78b934b22daea7b103ab714525
Mille Boström <mille_bo@hotmail.com>
2016-04-13 21:05:25 +0200

    Removed uneccesary variables and loops.

--- LetsCreatePokemon/Services/Windows/Message/MessagePage.cs
+++ LetsCreatePokemon/Services/Windows/Message/MessagePage.cs
@@ -7,59 +7,45 @@ namespace LetsCreatePokemon.Services.Windows.Message
     {
         private const int CharacterDelay = 200; 
         private readonly SpriteFont font;
-        private readonly char[] text;
+        private readonly string fullText;
         private readonly Vector2 position;
         private int index;
         private double counter;
-        private string currentText;
 
         public bool IsDone { get; set; }
 
         public MessagePage(string text, Vector2 position, SpriteFont font)
         {
-            this.text = text.ToCharArray();
+            this.fullText = text;
             this.position = position;
             this.font = font;
             index = 0;
             counter = 0;
-            currentText = "";
         }
 
         public void Update(double gameTime)
         {
-            if (index >= text.Length)
+            if (index >= fullText.Length)
                 return;
             counter += gameTime;
             if (counter > CharacterDelay)
             {
                 counter = 0;
                 index++;
-                if (index == text.Length - 1)
+                if (index == fullText.Length - 1)
                     IsDone = true;
-                UpdateText();
-            }
-        }
-
-        private void UpdateText()
-        {
-            currentText = "";
-            for (int n = 0; n < index; n++)
-            {
-                currentText += text[n];
             }
         }
 
         public void SpeedUpText()
         {
-            index = text.Length;
-            UpdateText();
+            index = fullText.Length;
             IsDone = true;
         }
 
-
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawString(font, currentText, position, Color.Gray);
+            spriteBatch.DrawString(font, fullText.Substring(0, index), position, Color.Gray);
         }
     }
 }
